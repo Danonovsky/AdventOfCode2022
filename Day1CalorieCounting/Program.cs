30c8d5ebe11a9119e5c062054fcad3ff3099ce1a
Daniel Józefiuk <daniel.jozefiuk@bcoders.pl>
2022-12-01 22:39:25 +0100

    Part 1 done

---
+++ Day1CalorieCounting/Program.cs
@@ -0,0 +1,14 @@
+﻿var lines = File.ReadLines("input.txt");
+var calories = new List<int>();
+var currentCalories = 0;
+foreach (var line in lines)
+{
+    if (line.Length is 0)
+    {
+        calories.Add(currentCalories);
+        currentCalories = 0;
+        continue;
+    }
+    currentCalories += int.Parse(line);
+}
+Console.WriteLine(calories.Max());
