14a9c22a6a5da4565b3f5aa71476355b8ffe7841
Daniel Józefiuk <daniel.jozefiuk@bcoders.pl>
2022-12-02 10:11:17 +0100

    Part 1 done

---
+++ Day2RockPaperScissors/Program.cs
@@ -0,0 +1,20 @@
+﻿var lines = File.ReadLines("input.txt");
+var score = 0;
+foreach (var line in lines)
+{
+    //A rock, B paper, C scissors
+    //X rock +1, Y paper +2, Z scissors +3
+    //0 lost, 3 draw, 6 win
+    score += line switch {
+        "A X" => 4,
+        "A Y" => 8,
+        "A Z" => 3,
+        "B X" => 1,
+        "B Y" => 5,
+        "B Z" => 9,
+        "C X" => 7,
+        "C Y" => 2,
+        "C Z" => 6
+    };
+}
+System.Console.WriteLine(score);
