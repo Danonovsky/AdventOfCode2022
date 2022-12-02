617210a74275c6d368511417cc93e841d1575773
Daniel Józefiuk <daniel.jozefiuk@bcoders.pl>
2022-12-02 10:12:45 +0100

    Day 2 done

--- Day2RockPaperScissors/Program.cs
+++ Day2RockPaperScissors/Program.cs
@@ -4,17 +4,17 @@ foreach (var line in lines)
 {
     //A rock, B paper, C scissors
     //X rock +1, Y paper +2, Z scissors +3
-    //0 lost, 3 draw, 6 win
+    //X lose 0, Y draw 3, Z win 6
     score += line switch {
-        "A X" => 4,
-        "A Y" => 8,
-        "A Z" => 3,
+        "A X" => 3,
+        "A Y" => 4,
+        "A Z" => 8,
         "B X" => 1,
         "B Y" => 5,
         "B Z" => 9,
-        "C X" => 7,
-        "C Y" => 2,
-        "C Z" => 6
+        "C X" => 2,
+        "C Y" => 6,
+        "C Z" => 7
     };
 }
 System.Console.WriteLine(score);
