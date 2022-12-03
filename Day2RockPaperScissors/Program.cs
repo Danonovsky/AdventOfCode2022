2cf616543eeea3f146440c6c997896e98a0b3432
Daniel Józefiuk <daniel.jozefiuk@bcoders.pl>
2022-12-03 11:20:51 +0100

    Upgraded day2, day3 init

--- Day2RockPaperScissors/Program.cs
+++ Day2RockPaperScissors/Program.cs
@@ -1,20 +1,15 @@
 ﻿var lines = File.ReadLines("input.txt");
-var score = 0;
-foreach (var line in lines)
+var score = lines.Sum(line => line switch
 {
-    //A rock, B paper, C scissors
-    //X rock +1, Y paper +2, Z scissors +3
-    //X lose 0, Y draw 3, Z win 6
-    score += line switch {
-        "A X" => 3,
-        "A Y" => 4,
-        "A Z" => 8,
-        "B X" => 1,
-        "B Y" => 5,
-        "B Z" => 9,
-        "C X" => 2,
-        "C Y" => 6,
-        "C Z" => 7
-    };
-}
-System.Console.WriteLine(score);
+    "A X" => 3,
+    "A Y" => 4,
+    "A Z" => 8,
+    "B X" => 1,
+    "B Y" => 5,
+    "B Z" => 9,
+    "C X" => 2,
+    "C Y" => 6,
+    "C Z" => 7,
+    _ => 0
+});
+Console.WriteLine(score);
---
+++ Day3RucksackReorganization/Program.cs
@@ -0,0 +1,7 @@
+﻿var lines = File.ReadLines("input.txt");
+var score = 0;
+foreach (var line in lines)
+{
+    Console.WriteLine(line);
+}
+Console.WriteLine(score);
