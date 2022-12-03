b36bca280620d5e065d850b2e75af0cc53d933a4
Daniel Józefiuk <daniel.jozefiuk@bcoders.pl>
2022-12-03 13:48:25 +0100

    Progress, counts wrong

--- Day3RucksackReorganization/Program.cs
+++ Day3RucksackReorganization/Program.cs
@@ -1,16 +1,31 @@
 ﻿var lines = File.ReadLines("input.txt");
 var score = 0;
+var dictionary = new Dictionary<char, int>();
+var i = 1;
 foreach (var line in lines)
 {
-    var firstCompartment = line[..(line.Length / 2)];
-    var secondCompartment = line[(line.Length / 2)..];
-    
-    foreach (var character in firstCompartment)
+    foreach (var character in line)
     {
-        if (secondCompartment.Contains(character) is false) continue;
+        if (dictionary.ContainsKey(character) is false)
+        {
+            dictionary.Add(character,1);
+            continue;
+        }
+        dictionary.TryGetValue(character, out var value);
+        if(value >= i) continue;
+        value++;
+        dictionary[character]=value;
+    }
+    i++;
+    if (i == 4)
+    {
+        var character = dictionary.Where(_ => _.Value == 3).Select(_ => _.Key).FirstOrDefault();
         var value = char.IsLower(character) ? character - 96 : character - 38;
+        Console.WriteLine($"{character}:{value}");
         score += value;
-        break;
+        i = 1;
+        dictionary.Clear();
     }
 }
 Console.WriteLine(score);
+
