048a7bc52a0cd4155760ea94e22c6c883c10d97d
Daniel Józefiuk <daniel.jozefiuk@bcoders.pl>
2022-12-03 13:17:05 +0100

    Part 1 done

--- Day3RucksackReorganization/Program.cs
+++ Day3RucksackReorganization/Program.cs
@@ -2,6 +2,15 @@
 var score = 0;
 foreach (var line in lines)
 {
-    Console.WriteLine(line);
+    var firstCompartment = line[..(line.Length / 2)];
+    var secondCompartment = line[(line.Length / 2)..];
+    
+    foreach (var character in firstCompartment)
+    {
+        if (secondCompartment.Contains(character) is false) continue;
+        var value = char.IsLower(character) ? character - 96 : character - 38;
+        score += value;
+        break;
+    }
 }
 Console.WriteLine(score);
