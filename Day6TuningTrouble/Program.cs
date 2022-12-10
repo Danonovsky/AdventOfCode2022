98f641929f12c8a189e1e09f917c35ae467ed3ab
Daniel Józefiuk <daniel.jozefiuk@bcoders.pl>
2022-12-10 13:30:29 +0100

    Day 6 done

---
+++ Day6TuningTrouble/Program.cs
@@ -0,0 +1,15 @@
+﻿var input = File.ReadAllLines("input.txt").First();
+const int detectSize = 14;
+for (var i = 13; i < input.Length; i++)
+{
+    var letters = new List<char>();
+    for (var j = i; j > i-detectSize; j--)
+    {
+        letters.Add(input[j]);
+    }
+    if (letters.Distinct().ToArray().Length == detectSize)
+    {
+        Console.WriteLine($"Signal found at {i+1}");
+        break;
+    }
+}
